6cb259d9c7e2960958fd51318f503968d5da2603
Jorsary <edikus29@gmail.com>
2022-12-06 18:48:13 +0300

    feat: notFound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 export const darkTheme = createTheme({
   palette: {
     mode: "dark",
@@ -16,9 +17,9 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
-
     </ThemeProvider>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <Box
+      sx={{
+        height: "75vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{
+          textAlign: "center",
+          fontWeight: 700,
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        Такой страницы не существует
+      </Typography>
+    </Box>
+  )
+}
+
+export default NotFound
