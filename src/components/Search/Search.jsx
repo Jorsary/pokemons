45fc48ec2add5817e2abaf02aa78241b5dd7f86e
Jorsary <edikus29@gmail.com>
2022-12-16 20:10:29 +0300

    feat: create hook useDeboucne

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,32 @@
 import CloseIcon from '@mui/icons-material/Close'
 import { IconButton, TextField } from '@mui/material'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useDebounce from '../../hooks/useDebounce'
 
 const Search = ({ searchValue, onChangeSearchValue }) => {
+  const [search, setSearch] = useState('')
+  const [debouncedValue] = useDebounce(search, 500)
   const onReset = () => {
     onChangeSearchValue('')
   }
 
+  useEffect(() => {
+    setSearch(searchValue)
+  }, [searchValue])
+
+  useEffect(() => {
+    if (debouncedValue) {
+      onChangeSearchValue(debouncedValue)
+    } else {
+      onChangeSearchValue('')
+    }
+  }, [debouncedValue])
+
   return (
     <TextField
-      value={searchValue}
-      onChange={(e) => { onChangeSearchValue(e.target.value) }}
+      value={search}
+      onChange={(e) => { setSearch(e.target.value) }}
       placeholder="Search"
       variant="standard"
       type='text'
